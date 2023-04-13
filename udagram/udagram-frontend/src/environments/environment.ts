cc6c4afcf86458f6dcb75fb07bc19f06d7fef2ec
Abdulaziz Alsafadi <azeez.h.749@gmail.com>
2023-04-13 14:30:26 +0300

    fixing localhost issues, fixing .elasticbeanstalk issue

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   appName: 'Udagram',
   apiHost: 'udagram-api-dev.us-east-1.elasticbeanstalk.com',
 };
-
+// udagram-api-dev.us-east-1.elasticbeanstalk.com
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
